2b11fe7626c4166656dfb82109917a348213189a
Chithambara Kumar <32110271+chithakumar13@users.noreply.github.com>
2020-08-15 03:41:13 +0530

    infinite grid

--- src/components/Home/Content/Content.component.tsx
+++ src/components/Home/Content/Content.component.tsx
@@ -1,12 +1,23 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { VideoGrid } from './VideoGrid/VideoGrid.component';
 import './Content.css';
-
+import { InfiniteScroll } from '../../InfiniteScroll/InfiniteScroll.component';
 
 export const Content = () => {
-    return <div className="video_content">
-        <div className="video_content_container">
-            <VideoGrid /><VideoGrid />
+
+    const [noOfGrids, setGrids] = useState(2);
+
+    const grids = Array.apply(null, new Array(noOfGrids)).map((val, index) => <VideoGrid key={index} />);
+
+    const onCallBack = () => {
+        setGrids((currentState) => currentState + 1);
+    }
+
+    return <InfiniteScroll callBack={onCallBack}>
+        <div className="video_content">
+            <div className="video_content_container">
+                {grids}
+            </div>
         </div>
-    </div>
+    </InfiniteScroll>
 }
--- src/components/Home/Content/VideoGrid/VideoGrid.component.tsx
+++ src/components/Home/Content/VideoGrid/VideoGrid.component.tsx
@@ -7,7 +7,7 @@ import { VideoGridHeader } from './VideoGridHeader/VideoGridHeader.component';
 
 export const VideoGrid = () => {
 
-    let previews = Array.apply(null,new Array(15)).map(() =><VideoPreview /> );
+    let previews = Array.apply(null,new Array(15)).map((val,index) =><VideoPreview key={index}/> );
     return <>
     <VideoGridHeader Title = "Test Title"></VideoGridHeader>
     <div className="video_grid">{previews}
